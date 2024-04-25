cd9ecb7268429e3ba553139fbe10f2c4e0a894c2
Yangfan <chenyangfan3211@gmail.com>
2024-04-25 17:58:05 +0800

    🎨 upgrad tailwind config

--- src/app/mint/page.tsx
+++ src/app/mint/page.tsx
@@ -1,7 +1,12 @@
 export default function MintPage() {
   return (
-    <div className="p-4 space-y-4 flex flex-col">
-      <p>This is mint page</p>
-    </div>
+    <>
+      <header className="fixed h-12 w-full px-4">Header</header>
+      <main className="bg-bgorange h-screen pt-12">
+        <div className="p-4 space-y-4 flex flex-col">
+          <p>This is mint page</p>
+        </div>
+      </main>
+    </>
   );
 }
