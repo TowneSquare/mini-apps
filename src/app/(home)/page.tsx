3b3218d9a465399b081f7e14e130033cf4b6349d
烦烦 <chenyangfan3211@gmail.com>
2024-04-28 01:42:49 +0800

    update home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,70 +10,71 @@ import SlothRight2Img from "../../../public/assets/home/sloth_right2.png";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-bgorange pt-5">
-      <Image
-        className="mx-auto"
-        src={TitleImg}
-        width={248}
-        height={144}
-        alt="title"
-      />
-      <Image
-        className="mx-auto mt-2"
-        src={LogoBg}
-        width={203}
-        height={32}
-        alt="Logo"
-      />
-      <div className="z-10 mt-5 flex flex-col items-center justify-center text-2xl font-black">
-        <span>Built with</span>
-        <span className="text-textpink">composability</span>
-        <span>
-          & <span className="text-textgreen">for mobile</span>
-        </span>
+    <main className="h-screen overflow-hidden bg-bgorange">
+      <div className="h-[40%] pt-5">
+        <div className="relative mx-auto h-[50%] w-3/4">
+          <Image src={TitleImg} fill={true} objectFit="contain" alt="title" />
+        </div>
+        <div className="relative mx-auto mt-2 h-[9%] w-3/4">
+          <Image
+            className=""
+            src={LogoBg}
+            fill={true}
+            objectFit="contain"
+            alt="Logo"
+          />
+        </div>
+        <div className="z-10 mt-5 flex h-[41%] flex-col items-center justify-center text-3xl font-black">
+          <span>Built with</span>
+          <span className="text-textpink">composability</span>
+          <span>
+            & <span className="text-textgreen">for mobile</span>
+          </span>
+        </div>
       </div>
-      <Image
-        className="fixed bottom-[9rem] left-1/2 -translate-x-2/4"
-        src={SlothImg}
-        width={360}
-        height={326}
-        alt="Sloth"
-      />
-      <div className="fixed -bottom-[9rem] h-80 w-[120%] -translate-x-[15%] rounded-tl-[90%] rounded-tr-[50%] bg-bggreen"></div>
-      <Image
-        className="fixed bottom-[1.5rem] left-[2.5rem]"
-        src={SlothLeft2Img}
-        width={180}
-        height={191}
-        alt="Sloth"
-      />
-      <Image
-        className="fixed bottom-0 left-0"
-        src={SlothLeft1Img}
-        width={114}
-        height={172}
-        alt="Sloth"
-      />
 
-      <Image
-        className="fixed bottom-8 left-1/2"
-        src={SlothRight2Img}
-        width={170}
-        height={175}
-        alt="Sloth"
-      />
-      <Image
-        className="fixed bottom-0 right-0"
-        src={SlothRight1Img}
-        width={126}
-        height={194}
-        alt="Sloth"
-      />
+      <div className="relative h-[60%]">
+        <div className="absolute top-0 h-[80%] w-full ">
+          <Image src={SlothImg} fill={true} objectFit="contain" alt="title" />
+        </div>
 
-      <div className="fixed left-1/2 bottom-8 h-20 w-11/12 -translate-x-2/4 rounded-2xl bg-sky-900 px-5 py-4">
-        <button className="h-full w-full rounded-xl bg-bggreen font-extrabold text-white">
-          <Link href="/mint">CONTINUE</Link>
-        </button>
+        <div className="absolute bottom-0 h-[30%] w-[120%] -translate-x-[15%] rounded-tl-[100%] rounded-tr-[50%] bg-bggreen"></div>
+        <Image
+          className="fixed bottom-[1.5rem] left-[2rem]"
+          src={SlothLeft2Img}
+          width={180}
+          height={196}
+          alt="Sloth"
+        />
+
+        <Image
+          className="fixed bottom-0 left-0"
+          src={SlothLeft1Img}
+          width={114}
+          height={172}
+          alt="Sloth"
+        />
+
+        <Image
+          className="fixed bottom-8 right-[1.5rem]"
+          src={SlothRight2Img}
+          width={170}
+          height={175}
+          alt="Sloth"
+        />
+        <Image
+          className="fixed bottom-0 right-0"
+          src={SlothRight1Img}
+          width={126}
+          height={194}
+          alt="Sloth"
+        />
+
+        <div className="fixed left-1/2 bottom-8 h-20 w-11/12 -translate-x-2/4 rounded-2xl bg-sky-900 px-5 py-4">
+          <button className="h-full w-full rounded-xl bg-bggreen font-extrabold text-white">
+            <Link href="/mint">CONTINUE</Link>
+          </button>
+        </div>
       </div>
     </main>
   );
