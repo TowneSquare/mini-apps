8d659f5462c9be9cf90c90d0160bdf554f80b4f1
mac <gos360@163.com>
2024-04-29 23:44:00 +0800

    feat: move image

--- src/app/hitbear/page.tsx
+++ src/app/hitbear/page.tsx
@@ -2,17 +2,22 @@
 import {useGSAP} from "@gsap/react"
 import gsap from "gsap"
 import bearImg from "../../../public/assets/game/Component 44.png"
+import React from "react"
 const HitBear =()=>{
     useGSAP(()=>{
-        gsap.to(".box",{x:250})
-    },[])
-
+    //     gsap.to(".box",{y:250})
+    // },[])
+    // 创建滑动动画
+    gsap.from('.box', { y: '100%', duration: 1, ease: 'power4.out' });
+}, [])
     return (
-        <div className="">
-            <div className="box w-20 h-20 bg-red-300"></div>
-            <img className="items-center" src={bearImg.src} alt=""/>
+        <div className="flex justify-center h-screen overflow-hidden">
+
+            {/*<div className="box w-20 h-20 bg-red-300" />*/}
+            <img className=" box items-center" src={bearImg.src} alt="" />
 
         </div>
+
     )
 }
 
