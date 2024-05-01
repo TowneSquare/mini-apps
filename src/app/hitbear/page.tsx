1732846bf475b2026ac9ae06563d2c0fbbf8a75e
mac <gos360@163.com>
2024-05-01 12:04:26 +0800

    add:go on

--- src/app/hitbear/page.tsx
+++ src/app/hitbear/page.tsx
@@ -4,6 +4,10 @@ import bearStartImg from "../../../public/assets/game/Untitled_Artwork 14 1.png"
 import num30Img from "../../../public/assets/game/30.png"
 import num01Img from "../../../public/assets/game/01.png"
 import hitImg from "../../../public/assets/game/hithim.png"
+import healthImg from "../../../public/assets/game/Component 20-1.png"
+import logoImg from "../../../public/assets/game/Frame 48096883-2.png"
+import hitBearImg from "../../../public/assets/game/Component 44.png"
+import blinkBearImg from "../../../public/assets/game/Component 44-1.png"
 
 import frontImg from "../../../public/assets/game/Frame 48096887-1.png"
 import bgImg from "../../../public/assets/game/bg.svg"
@@ -12,21 +16,24 @@ import Image from "next/image";
 
 const HitBear = () => {
 
-    const imgRef = useRef(null);
+    const bearRef = useRef(null);
     const [loaded, setLoaded] = useState(false);
     const num30Ref = useRef(null);
     const num01Ref = useRef(null);
     const hitRef = useRef(null);
+    const hitBearRef = useRef(null);
+    const healthRef = useRef(null);
+    const blinkBearRef = useRef(null);
 
     useEffect(() => {
-        if (imgRef.current) {
-            gsap.set(imgRef.current, {
+        if (bearRef.current) {
+            gsap.set(bearRef.current, {
                 y: '29vh',
             });
         }
-        if (loaded && imgRef.current) {
+        if (loaded && bearRef.current) {
             console.log("触发图片")
-            gsap.to(imgRef.current, {
+            gsap.to(bearRef.current, {
                 duration: 2,
                 scale: 1.41,
                 y: '2vh',
@@ -42,6 +49,11 @@ const HitBear = () => {
             tl.to(num01Ref.current, {opacity: 0})
             tl.to(hitRef.current, {opacity: 1, duration: 1})
             tl.to(hitRef.current, {opacity: 0})
+            tl.to(bearRef.current, {opacity: 0,duration: 0})
+            tl.to(blinkBearRef.current, {opacity: 1,duration: 0})
+            tl.to(blinkBearRef.current, {opacity: 0,duration: 0})
+            tl.to(hitBearRef.current, {opacity: 1,duration: 0})
+
         }
     }, [loaded]);
 
@@ -65,20 +77,24 @@ const HitBear = () => {
             {/* 开始图片 */}
             <img className={`absolute bottom-0 z-20 w-full object-cover ${loaded ? 'hidden' : ''}`}
                  src={bearStartImg.src} alt=""/>
-            <img ref={num30Ref} className={`absolute top-52 z-20 w-full object-cover w-2/3 `}
+            <img ref={healthRef} className={`absolute top-0 z-20 w-full object-cover  `}
+                 src={logoImg.src} alt=""/>
+            <img ref={healthRef} className={`absolute top-20 z-20 w-3/4 object-cover  `}
+                 src={healthImg.src} alt=""/>
+            <img ref={num30Ref} className={`absolute top-52 z-20 w-3/4 object-cover  `}
                  src={num30Img.src} alt=""/>
-            <img ref={num01Ref} className={`absolute top-52 z-20 w-full object-cover w-2/3 opacity-0 `}
+            <img ref={num01Ref} className={`absolute top-52 z-20 w-3/4 object-cover  opacity-0 `}
                  src={num01Img.src} alt=""/>
-            <img ref={hitRef} className={`absolute top-52 z-20 w-full object-cover w-2/3 opacity-0 `}
+            <img ref={hitRef} className={`absolute top-52 z-20 w-3/4 object-cover  opacity-0 `}
                  src={hitImg.src} alt=""/>
-            {/* <img ref={imgRef} */}
+            {/* <img ref={bearRef} */}
             {/*        src={bearImg.src} */}
             {/*        alt="Description" */}
             {/*        onLoad={handleImageLoad} */}
             {/*        onError={handleImageError} */}
             {/*        className={`w-full h-full object-contain ${loaded ? '' : 'hidden'}`} /> */}
             {/* 移动图片 */}
-            <Image ref={imgRef}
+            <Image ref={bearRef}
                    src={bearStartImg.src}
                    alt="Description"
                    onLoad={handleImageLoad}
@@ -87,6 +103,16 @@ const HitBear = () => {
                    height={300}
                    loading="eager"
                    className={`w-full h-full object-contain ${loaded ? '' : 'hidden'}`}/>
+            <img ref={blinkBearRef}
+                 src={blinkBearImg.src}
+                 alt="Description"
+                 className={`absolute top-52 z-20 w-full object-cover  opacity-0 `}/>
+
+            <img ref={hitBearRef}
+                 src={hitBearImg.src}
+                 alt="Description"
+                 className={`absolute top-52 z-20 w-full object-cover  opacity-0 `}/>
+
 
         </div>
 
