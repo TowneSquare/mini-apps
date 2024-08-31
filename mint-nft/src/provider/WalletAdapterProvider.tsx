d82bdc004aade7be518cb2977c89cfa4df6ac7ed
Pelumi <fatolupelumi@gmail.com>
2024-08-31 12:02:54 +0100

    added Martain and OKx wallet

--- mint-nft/src/provider/WalletAdapterProvider.tsx
+++ mint-nft/src/provider/WalletAdapterProvider.tsx
@@ -3,9 +3,13 @@ import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 // wallets
 import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { PontemWallet } from "@pontem/wallet-adapter-plugin";
+import { OKXWallet } from "@okwallet/aptos-wallet-adapter";
+import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
+
+
 // import { WalletConnector } from "@aptos-labs/wallet-adapter-mui-design";
 // define the wallets
-const theWallets = [new PetraWallet(), new PontemWallet()];
+const theWallets = [new PetraWallet(), new PontemWallet(),new OKXWallet(), new MartianWallet(),];
 export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <AptosWalletAdapterProvider
