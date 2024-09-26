2184682862b1352011aa338397551f603cecfc6a
Pelumi <fatolupelumi@gmail.com>
2024-09-26 21:01:07 +0100

    nhomepage now evolve screen

--- battle-game/src/app/(battle-game)/page.tsx
+++ battle-game/src/app/(battle-game)/page.tsx
@@ -9,6 +9,7 @@ import { useSlothBallData } from "@/src/hooks";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useCountdown } from "@/src/hooks/useCountdown";
 import { GAME_START_TIME } from "@/src/config/constants";
+import EvolvePage from "./evolve/page";
 
 export default function BattleGamePage() {
   const { account, connected } = useWallet();
@@ -23,52 +24,55 @@ export default function BattleGamePage() {
  
 
   return (
+    // <>
+    //   <main className="w-full h-full pb-10 bg-no-repeat bg-contain bg-oval-pattern">
+    //     <CommonPageHeader />
+    //     <div className="flex flex-col items-center justify-center mt-3">
+    //       {/* <p className="text-xl text-white">JamseX99,</p> */}
+    //       <p className="text-3xl font-bold text-[#8be5d4]">
+    //         Defeat the Evil Sloth
+    //       </p>
+    //       <p className="text-3xl font-bold text-white">
+    //         to evolve your Slothball!
+    //       </p>
+    //     </div>
+
+    //     <div className="flex items-center justify-center mt-16">
+    //       <Image
+    //         src={EvilAndSloths}
+    //         className="md:h-[493px] md:w-[426px]"
+    //         alt="evil-and-sloths"
+    //         style={{ objectFit: "contain" }}
+    //       />
+    //     </div>
+
+    //     <BattleCard
+    //       eligible={eligible}
+    //       gameStartTime={GAME_START_TIME}
+    //       evolveNumber={slothBallData.data?.length}
+    //       connected={connected}
+    //     />
+    //     {GAME_START_TIME - Math.floor(Date.now()) <= 30000 && !(GAME_START_TIME - Math.floor(Date.now()) <= 0) ? (
+    //       <RouteButton
+    //         disabled={!eligible}
+    //         title={`Battle ${timeInfo.secondsStr}`}
+            
+    //         bottomClass="bottom-8"
+    //         gameStartTime={GAME_START_TIME}
+    //       />
+    //     ) : (
+    //       <RouteButton
+    //         disabled={!eligible}
+    //         title={`Battle`}
+            
+    //         bottomClass="bottom-8"
+    //         gameStartTime={GAME_START_TIME}
+    //       />
+    //     )}
+    //   </main>
+    // </>
     <>
-      <main className="w-full h-full pb-10 bg-no-repeat bg-contain bg-oval-pattern">
-        <CommonPageHeader />
-        <div className="flex flex-col items-center justify-center mt-3">
-          {/* <p className="text-xl text-white">JamseX99,</p> */}
-          <p className="text-3xl font-bold text-[#8be5d4]">
-            Defeat the Evil Sloth
-          </p>
-          <p className="text-3xl font-bold text-white">
-            to evolve your Slothball!
-          </p>
-        </div>
-
-        <div className="flex items-center justify-center mt-16">
-          <Image
-            src={EvilAndSloths}
-            className="md:h-[493px] md:w-[426px]"
-            alt="evil-and-sloths"
-            style={{ objectFit: "contain" }}
-          />
-        </div>
-
-        <BattleCard
-          eligible={eligible}
-          gameStartTime={GAME_START_TIME}
-          evolveNumber={slothBallData.data?.length}
-          connected={connected}
-        />
-        {GAME_START_TIME - Math.floor(Date.now()) <= 30000 && !(GAME_START_TIME - Math.floor(Date.now()) <= 0) ? (
-          <RouteButton
-            disabled={!eligible}
-            title={`Battle ${timeInfo.secondsStr}`}
-            
-            bottomClass="bottom-8"
-            gameStartTime={GAME_START_TIME}
-          />
-        ) : (
-          <RouteButton
-            disabled={!eligible}
-            title={`Battle`}
-            
-            bottomClass="bottom-8"
-            gameStartTime={GAME_START_TIME}
-          />
-        )}
-      </main>
+    <EvolvePage/>
     </>
   );
 }
